bbef8c66072b69b845e394ed19e5f93e366a95bf
Irwiing <irwingtguarnieri@gmail.com>
2021-03-20 13:44:07 -0300

    Add project files.

---
+++ ProjetoRefeicao/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ProjetoRefeicao.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ ProjetoRefeicao/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjetoRefeicao.WebApiApplication" Language="C#" %>
